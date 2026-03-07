0dae84387590305ed7d08b74ac0a92a32f60eba7
Tsehan <yourmortyhan52@gmail.com>
2026-03-07 21:11:54 +0300

    иницилизация проекта

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import StartPage from './components/StartPage'
+import ButtonToTab from './components/ButtonToTab'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <StartPage />
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/StartPage.jsx
@@ -0,0 +1,11 @@
+import ButtonToTab from './ButtonToTab'
+
+
+export default function StartPage() {
+    return (
+        <>
+            <h1>Your Web Space</h1>
+            <ButtonToTab children={'START'}></ButtonToTab>
+        </>
+    )
+}
